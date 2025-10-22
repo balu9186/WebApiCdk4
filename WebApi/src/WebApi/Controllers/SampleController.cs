44fcc5e8b0295e4a1023ccab93fb258493ba45f5
Bandreddi, Balaji <BBandreddi+verisk@verisk.com>
2025-10-22 17:31:14 -0400

    Adde Sample controller

---
+++ WebApi/src/WebApi/Controllers/SampleController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        // GET: api/<SampleController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var items = new string[] { "value1", "value2" };
+            return Ok(items);
+        }
+
+        // GET api/<SampleController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SampleController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SampleController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SampleController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
